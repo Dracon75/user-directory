af229317947347f7a98ffc8a66a12e13ad43a6e8
Dracon75 <nanorth75@gmail.com>
2021-06-27 16:05:13 -0400

    initial commit

---
+++ src/utils/API.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+//retrieve randomuser api
+const API = {
+  getApi: function () {
+    return axios.get("https://randomuser.me/api/?results=200&nat=us");
+  }
+}
+
+export default API;
+
+
